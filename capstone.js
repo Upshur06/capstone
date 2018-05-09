6dc549af2adb759cc0fcbfbba29c6694ead72c11
Upshur06 <mupshur2005@gmail.com>
2018-05-09 17:49:44 -0400

    first_commit

---
+++ capstone.js
@@ -0,0 +1,27 @@
+
+
+// var wrapper = document.createElement("div")
+//               wrapper.style.width = "100%"
+//               wrapper.style.height = "700px"
+//               wrapper.style.border = "3px solid"
+//               document.body.appendChild(wrapper)
+//
+// var sports = document.createElement("div")
+//             sports.style.width = "100px";
+//             sports.style.height = "80px";
+//             sports.style.backgroundColor = "blue";
+//             sports.style.border = "2px solid"
+//             sports.innerHTML = "Sports";
+//             sports.style.fontSize = "25px";
+//             sports.style.textTransform = "uppercase";
+//             sports.style.textAlign = "center"
+//             sports.style.postion = "relative";
+//             sports.style.position =
+//             document.body.appendChild(wrapper)
+
+
+function sport(array){
+  var list = ["jersey", "football", "gatorade", "coaches"]
+
+
+}
