72c721315c94a1067b0b4e95414f1d8af399a6f8
Upshur06 <mupshur2005@gmail.com>
2018-05-24 23:28:34 -0400

    checkingIn

--- capstone.js
+++ capstone.js
@@ -1,13 +1,64 @@
 // array for the sports column
-var list = ["jersey", "football", "gatorade", "coaches"];
-var part = ["What is the uniform called that players wear in a game?",
-            "What does the QB throw or hand off to another player?",
-            "What is the name of the drink that's used mostly in sports?",
-            "Who are responsible for teaching players how to play the game?"];
+function word(sportsList, hintList){
+  var sportsList = sportsListArray.push(sportsList)
+  var hintList = hintListArray.push(hintList)
+}
 
-var random = Math.floor(Math.random() * list.length);
-var tempItem = list[random]
-var dashes = tempItem.replace(/[a-z]/gi, "   __   ")
+var sportsListArray = [];
+var hintListArray = [];
+var randomizer = 0;
+var splitSportsWord;
+var wear = new word("jersey", "What is the uniform called that players wear in a game?");
+var ball = new word("football", "What does the QB throw or hand off to another player?");
+var drink = new word("gatorade", "What is the name of the drink that's used mostly in sports?");
+var job = new word("coaches", "Who are responsible for teaching players how to play the game?");
+
+var hero = new word("batman", "What Caped Crusader is known as the Dark Knight?");
+var country = new word("wakanda", "T'Challa is a superhero character, who is king of what country?");
+var color = new word("grey", "What color was the Hulk when he was originally introduced in Marvel Comics?");
+var metal = new word("adamantium", "What type of metal is Wolverine's claws made of?");
+
+var little = new word("short", "What five-letter word becomes shorter when you add two letters to it?");
+var letter = new word("envelope", "What word begins and ends with an E but only has one letter?");
+var paper = new word("paper", "You can drop me from the tallest building and I'll be fine, but if you drop me in water I die. What am I??");
+var eat = new word("meat", "Paul's height is six feet, he's an assistant at a butcher's shop, and wears size 9 shoes. What does he weigh?");
+
+var sport = new word("baseball", "Jackie Robinson became the first African American to play in Major League _____?");
+var church = new word("ebenezer", "What was the name of Dr. Martin Luther King Jr.'s father's church?");
+var city = new word("chicago", "In what Midwestern city did Dr. Hale Williams establish the first African-American hospital?");
+var politics = new word("obama", "Who is the first African American President of the U.S.");
+
+
+// hintList chance()
+function chance(){
+    return (Math.floor(Math.random() * sportsListArray.length));
+}
+var randomSportsWord = chance();
+
+// create a split function
+// var splitSportsWord = sportsListArray[randomizer].split("")
+
+// console.log(splitSportsWord)
+var sportsArray = [];
+
+function collection(dashesArray){
+  var checkArray = sportsArray.push(checkArray)
+
+}
+collection(dashesArray)
+
+var dashesArray = [];
+// create a string dash function
+function stringDash(arg){
+  dashesArray = [];
+    for(i=0;i<arg.length;i++){
+      dashesArray.push(" __ ");
+    }
+    var dashes = dashesArray.join('')
+    return dashes
+}
+
+// stringDash(splitSportsWord)
 
 
 //wrapper for the entire page in the black border
@@ -15,6 +66,8 @@ var wrapper = document.createElement("div")
               wrapper.style.width = "100%"
               wrapper.style.height = "850px"
               wrapper.style.border = "3px solid"
+              wrapper.style.postion= "relative"
+              wrapper.style.top = "10px"
               document.body.appendChild(wrapper)
 
 
@@ -22,12 +75,12 @@ var wrapper = document.createElement("div")
 var subject = document.createElement("div")
               subject.style.width = "160px";
               subject.style.height = "50px";
-              subject.style.border = "2px solid";
-              subject.style.backgroundColor = "blue"
+              subject.style.border = "4px solid";
+              subject.style.backgroundColor = "#0066cc"
               subject.style.position = "relative";
               subject.style.top = "10px";
               subject.style.left = "360px";
-              subject.innerHTML =      "categories";
+              subject.innerHTML =  "categories";
               subject.style.fontSize = "25px";
               subject.style.textTransform = "uppercase";
               subject.style.textAlign = "center";
@@ -36,53 +89,47 @@ var subject = document.createElement("div")
 // The 4 options button below the category
 var categories = ["sports", "comics", "riddles", "history"];
 
+
 for(let i=0; i<4; i++){
     var topics = document.createElement("button")
                 topics.style.width = "100px";
                 topics.style.height = "45px";
                 topics.style.position = "relative";
                 topics.style.top = "30px";
-                topics.style.left = "210px";
+                topics.style.left = "190px";
                 topics.style.marginLeft = "15px"
                 topics.style.padding = "10px";
-                topics.style.backgroundColor = "blue";
+                topics.style.backgroundColor = "#0066cc";
                 topics.style.border = "2px solid";
                 topics.style.fontSize = "20px";
                 topics.style.textTransform = "uppercase";
+                topics.style.textDecoration = "underline"
                 topics.style.textAlign = "center";
-                topics.style.float = "left";
+                topics.style.float = "";
                 topics.classList.add("sports")
                 topics.innerHTML = categories[i];
-                // sports.style.postion = "relative";
-                // sports.style.pos =
                 wrapper.appendChild(topics)
-
 }
 
-
 //The green border for Alphabet letters and the guessing letters
-
 var box = document.createElement("div")
-          box.style.width = "70%";
-          box.style.height = "600px";
+          box.style.width = "680px";
+          box.style.height = "630px";
           box.style.border = "2px solid green";
           box.style.position = "relative";
-          box.style.top = "120px";
+          box.style.top = "100px";
           box.style.left = "120px";
           wrapper.appendChild(box)
 
-
 //  The orange box for the Alphabet buttons
-
 var wrap = document.createElement("div")
-            wrap.style.width = "60%"
-            wrap.style.height = "345px"
-            wrap.style.border = "2px solid orange"
+            wrap.style.width = "400px"
+            wrap.style.height = "325px"
+            // wrap.style.border = "2px solid orange"
             wrap.style.position = "relative";
-            wrap.style.top = "10px";
+            wrap.style.top = "50px";
             wrap.style.left = "110px";
             box.appendChild(wrap)
-
 
 
 //The individual Alphabet buttons
@@ -97,100 +144,116 @@ for(let i=0; i<26; i++){
                 letters.style.left = "0px";
                 letters.style.marginLeft = "18px"
                 letters.style.marginTop = "5px"
-                // letters.style.padding = "5px";
-                // letters.style.backgroundColor = "blue";
                 letters.style.border = "2px solid navy";
                 letters.style.fontSize = "15px";
                 letters.style.textTransform = "uppercase";
                 letters.style.borderRadius = "20%"
                 letters.style.textAlign = "center";
                 letters.style.float = "left";
-                // letters.style.
                 letters.classList.add("a")
                 letters.innerHTML = alphabet[i];
-                // sports.style.postion = "relative";
-                // sports.style.pos =
                 wrap.appendChild(letters);
                 var selector = document.getElementsByClassName("a")
-                    selector[i].addEventListener("click", function(event){
-                      type.innerHTML = event.target.innerHTML
+                    selector[i].addEventListener('click', function(event){
+                      for(let i = 0;i<splitSportsWord.length;i++){
+                        if(event.target.innerHTML === splitSportsWord[i]){
+                          rightGuessFunction(event.target.innerHTML)
+                        }
+                      }
+                      wrongGuessFunction(event.target.innerHTML)
+                      winOrLose()
                     })
-
 }
 
-// The red box where the letter will be keyed in from selecting a letter from the alphabets
-var type = document.createElement("div")
-            type.style.width = "50%";
-            type.style.height = "40px";
-            type.style.border = "1px solid red";
-            type.style.position = "relative";
-            type.style.top = "20px";
-            type.style.left = "150px";
-            box.appendChild(type)
+function wrongGuessFunction(x){
+  wrongGuess.innerHTML += x
+}
 
 
+function rightGuessFunction(letter){
+    for(let i =0; i < splitSportsWord.length; i++){
+      if(letter === splitSportsWord[i]){
+        dashesArray.splice(i,1, letter)
+      }
+    }
+    answer.innerHTML = dashesArray.join('')
+}
 
-// Purple box where the number of dashes will be present
+
+// The red box where the wrong guess letter will be stored
+var wrongGuess = document.createElement("div")
+            wrongGuess.style.width = "430px";
+            wrongGuess.style.height = "40px";
+            wrongGuess.style.border = "1px solid red";
+            wrongGuess.style.position = "relative";
+            wrongGuess.style.textAlign = "center"
+            wrongGuess.style.fontSize = "25px"
+            wrongGuess.style.top = "60px";
+            wrongGuess.style.left = "83px";
+            wrongGuess.style.input = "onfocus"
+            box.appendChild(wrongGuess)
+
+
+// // Purple box where the number of dashes will be present
 var answer = document.createElement("div")
-            answer.style.width = "70%";
+            answer.style.width = "480px";
             answer.style.height = "50px";
             answer.style.border = "4px solid purple";
             answer.style.position = "relative";
-            answer.innerHTML = dashes;
             answer.style.textAlign = "center"
-            answer.style.top = "40px";
-            answer.style.left = "80px";
+            answer.style.fontSize = "35px"
+            answer.style.top = "70px";
+            answer.style.left = "60px";
             box.appendChild(answer)
 
 
-// array to get the list of letters to corrospond the correct letter
-
-var correct = [];
-var wrong = [];
-
-function guessing(letter){
-    for(i=0; i<dashes.length[i]; i++){
-      if(letter === dashes[i]){
-        console.log(correct.push[dashes[i]])
-      }{
-        // wrong.push[dashes[i]]
-      }
-    }
-}
-        // console.log(t)
-
-function flip(){
-    for(i=0; i<dashes.length[i]; i++){
-      str = dashes.replace("__", "alphabet[i]")
-  }
-}
 // Blue box that will prompt hint
 var questionaire = document.createElement("div")
-                  questionaire.style.width = "80%";
-                  questionaire.style.height = "80px";
-                  questionaire.style.border = "5px solid blue";
-                  questionaire.innerHTML = part[random]
+                  questionaire.style.width = "500px";
+                  questionaire.style.height = "100px";
+                  questionaire.style.border = "5px solid #0066cc";
                   questionaire.style.position = "relative";
                   questionaire.style.textAlign = "center"
                   questionaire.style.fontSize = "20px"
-                  questionaire.style.top = "55px";
-                  questionaire.style.left = "50px";
+                  questionaire.style.top = "85px";
+                  questionaire.style.left = "55px";
                   box.appendChild(questionaire)
 
+//next button
+function nextButton(){
+    var arrow = document.createElement("button")
+                arrow.style.width = "50px"
+                arrow.style.height = "40px"
+                arrow.style.border = "4px solid"
+                arrow.style.position = "relative"
+                arrow.style.bottom = "30px"
+                arrow.style.left = "595px"
+                arrow.innerHTML = "next"
+                arrow.style.fontSize = "12px"
+                arrow.style.textTransform = "uppercase"
+                arrow.style.textAlign = "center";
+                box.appendChild(arrow)
 
-var sports = document.getElementsByClassName("sports")[0]
-      sports.addEventListener("click", function(){
+                arrow.addEventListener("click", hinting)
+}
+nextButton()
 
-      });
-// var comics = document.getElementsByClassName("comics")
-//       comics.addEventListener("click", function(){
-//
-//       });
-// var riddles = document.getElementsByClassName("riddles")
-//       riddles.addEventListener("click", function(){
-//
-//       });
-// var history = document.getElementsByClassName("history")
-//       history.addEventListener("click", function(){
-//
-//       });
+function hinting(){
+          randomizer = (Math.floor(Math.random() * sportsListArray.length));
+          splitSportsWord = sportsListArray[randomizer].split("")
+          answer.innerHTML= stringDash(splitSportsWord);
+          questionaire.innerHTML = hintListArray[randomizer];
+          wrongGuess.innerHTML =  wrongGuess.value = " "
+}
+
+
+
+alert("Click the next button to start the game")
+
+function winOrLose(){
+        if(dashesArray.join('') === splitSportsWord.join('')){
+            setTimeout(function(){
+              alert("Congrats,...You Won")
+            }, 1000)
+        }
+}
