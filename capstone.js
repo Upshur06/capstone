9a78f3ff7ac4fa36b74708abf85ff374ced144ef
Upshur06 <mupshur2005@gmail.com>
2020-06-18 00:56:32 -0400

    flexboxLetters.innerHTML

--- capstone.js
+++ capstone.js
@@ -5,65 +5,68 @@ let dashesLetter = document.getElementById("dashesLetter");
 let questionaire = document.getElementById("questionaire");
 let nextArrow = document.getElementById("arrow");
 
+let sportsListArray = [];
+let hintListArray = [];
+let randomizer = 0;
+let splitSportsWord;
+let wrongGuessCount = 0;
+
 // array for the catagory column
-// function word(catagoryList, hintList){
-//   var catagoryList = sportsListArray.push(sportsList)
-//   var hintList = hintListArray.push(hintList)
-// }
+function word(catagoryList, hintList){
+  var catagoryList = sportsListArray.push(catagoryList)
+  var hintList = hintListArray.push(hintList)
+}
 
-// var sportsListArray = [];
-// var hintListArray = [];
-// var randomizer = 0;
-// var splitSportsWord;
-// var wrongGuessCount = 0;
 
-// var wear = new word("jersey", "What is the uniform called that players wear in a game?");
-// var ball = new word("football", "What does the QB throw or hand off to another player?");
-// var drink = new word("gatorade", "What is the name of the drink that's used mostly in sports?");
-// var job = new word("coaches", "Who are responsible for teaching players how to play the game?");
+let wear = new word("jersey", "What is the uniform called that players wear in a game?");
+let ball = new word("football", "What does the QB throw or hand off to another player?");
+let drink = new word("gatorade", "What is the name of the drink that's used mostly in sports?");
+let job = new word("coaches", "Who are responsible for teaching players how to play the game?");
 
-// var hero = new word("batman", "What Caped Crusader is known as the Dark Knight?");
-// var country = new word("wakanda", "T'Challa is a superhero character, who is king of what country?");
-// var color = new word("grey", "What color was the Hulk when he was originally introduced in Marvel Comics?");
-// var metal = new word("adamantium", "What type of metal is Wolverine's claws made of?");
+// let hero = new word("batman", "What Caped Crusader is known as the Dark Knight?");
+// let country = new word("wakanda", "T'Challa is a superhero character, who is king of what country?");
+// let color = new word("grey", "What color was the Hulk when he was originally introduced in Marvel Comics?");
+// let metal = new word("adamantium", "What type of metal is Wolverine's claws made of?");
 
-// var little = new word("short", "What five-letter word becomes shorter when you add two letters to it?");
-// var letter = new word("envelope", "What word begins and ends with an E but only has one letter?");
-// var paper = new word("paper", "You can drop me from the tallest building and I'll be fine, but if you drop me in water I die. What am I??");
-// var eat = new word("meat", "Paul's height is six feet, he's an assistant at a butcher's shop, and wears size 9 shoes. What does he weigh?");
+// let little = new word("short", "What five-letter word becomes shorter when you add two letters to it?");
+// let letter = new word("envelope", "What word begins and ends with an E but only has one letter?");
+// let paper = new word("paper", "You can drop me from the tallest building and I'll be fine, but if you drop me in water I die. What am I??");
+// let eat = new word("meat", "Paul's height is six feet, he's an assistant at a butcher's shop, and wears size 9 shoes. What does he weigh?");
 
-// var sport = new word("baseball", "Jackie Robinson became the first African American to play in Major League _____?");
-// var church = new word("ebenezer", "What was the name of Dr. Martin Luther King Jr.'s father's church?");
-// var city = new word("chicago", "In what Midwestern city did Dr. Hale Williams establish the first African-American hospital?");
-// var politics = new word("obama", "Who is the first African American President of the U.S.");
+// let sport = new word("baseball", "Jackie Robinson became the first African American to play in Major League _____?");
+// let church = new word("ebenezer", "What was the name of Dr. Martin Luther King Jr.'s father's church?");
+// let city = new word("chicago", "In what Midwestern city did Dr. Hale Williams establish the first African-American hospital?");
+// let politics = new word("obama", "Who is the first African American President of the U.S.");
 
 
 // hintList chance()
-// function chance(){
+// let randomSportsWord = function chance(){
 //     return (Math.floor(Math.random() * sportsListArray.length));
 // }
-// var randomSportsWord = chance();
+
 
 // create a split function
-// var splitSportsWord = sportsListArray[randomizer].split("")
+ // splitSportsWord = sportsListArray[randomizer].split("")
 
 // console.log(splitSportsWord)
-// var sportsArray = [];
+// let sportsArray = [];
 
 // function collection(dashesArray){
-//   var checkArray = sportsArray.push(checkArray)
+//   let checkArray = sportsArray.push(checkArray)
 //
 // }
+
 // collection(dashesArray)
 
-// var dashesArray = [];
+// let dashesArray = [];
+
 // create a string dash function
 // function stringDash(arg){
 //   dashesArray = [];
 //     for(i=0;i<arg.length;i++){
 //       dashesArray.push(" __ ");
 //     }
-//     var dashes = dashesArray.join('')
+//     let dashes = dashesArray.join('')
 //     return dashes
 // }
 
@@ -73,27 +76,24 @@ function testDrive(){
 
     for(let i=0; i<flexboxLetters.length; i++){
         flexboxLetters[i].addEventListener("click", function(event){
-            console.log(flexboxLetters[i].innerHTML)
-        })
+          wrongGuessLetter.innerHTML += flexboxLetters[i].innerHTML;
 
-                // var selector = document.getElementsByClassName("a")
-                //     selector[i].addEventListener('click', function(event){
-                //       for(let i = 0;i<splitSportsWord.length;i++){
-                //         if(event.target.innerHTML === splitSportsWord[i]){
-                //           rightGuessFunction(event.target.innerHTML)
-                //         }
-                //       }
-                //       wrongGuessFunction(event.target.innerHTML)
-                //       winOrLose()
-                //     })
-    }
-}
+              // for(let i = 0;i<splitSportsWord.length;i++){
+              //       if(event.target.innerHTML === splitSportsWord[i]){
+              //           rightGuessFunction(event.target.innerHTML)
+              //       }
+              //  }
+              //           wrongGuessFunction(event.target.innerHTML)
+              //           winOrLose()
+         })
+     }
+ }
 
 // function wrongGuessFunction(x){
-//   wrongGuess.innerHTML += x
+//   wrongGuessLetter.innerHTML += x
 //   alert("Incorrect")
 //   wrongGuessCount += 1;
-  // console.log('Wrong guesses:', wrongGuessCount);
+//   console.log('Wrong guesses:', wrongGuessCount);
 //   if (wrongGuessCount >= 5) {
 //     Lose();
 //   }
@@ -112,7 +112,7 @@ function testDrive(){
 
 //next button
 // function nextButton(){
-//     var arrow = document.createElement("button")
+//     let arrow = document.createElement("button")
 //                 arrow.addEventListener("click", hinting)
 //
 // }
